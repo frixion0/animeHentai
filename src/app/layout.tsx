5211f8cc4501fa5a5f3a2d028cae4deb5d116301
Varalakshmi A <varalakshmilvp@gmail.com>
2025-10-01 06:35:36 +0000

    Sorry gave the wrong ad code this is the original replace previous to 5h

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet" />
-        <script type='text/javascript' src='//pl27758254.revenuecpmgate.com/4e/bc/68/4ebc68fc4fa5354e73e4cb4b414b70ec.js'></script>
+        <script type='text/javascript' src='//hollowsuspicion.com/4e/bc/68/4ebc68fc4fa5354e73e4cb4b414b70ec.js'></script>
       </head>
       <body className="font-body antialiased">
         {children}
