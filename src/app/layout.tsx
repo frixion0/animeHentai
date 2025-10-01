a81294e93e6ff0a13de4726cdf2920c089f556af
Varalakshmi A <varalakshmilvp@gmail.com>
2025-10-01 09:25:23 +0000

    Also add this native banner ad <script async="async" data-cfasync="false

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet" />
         <script type='text/javascript' src='//hollowsuspicion.com/4e/bc/68/4ebc68fc4fa5354e73e4cb4b414b70ec.js'></script>
+        <script async="async" data-cfasync="false" src="//hollowsuspicion.com/559c3ecf549e40ac506159daa6990f26/invoke.js"></script>
       </head>
       <body className="font-body antialiased">
         {children}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 
 export default function Home() {
   return (
-    <main className="h-screen w-screen overflow-hidden bg-background">
+    <main className="h-screen w-screen overflow-hidden bg-background flex flex-col">
+      <div id="container-559c3ecf549e40ac506159daa6990f26" className="w-full"></div>
       <iframe
         src="https://animehentai.art/"
         title="Seamless Viewer"
