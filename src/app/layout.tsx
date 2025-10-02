bf123a41cb9ad0fa43a9dea6ae80509a448bde13
Varalakshmi A <varalakshmilvp@gmail.com>
2025-10-02 23:35:44 +0000

    Ok now check if these ads exist if not add them and make sure the websit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,52 @@ export default function RootLayout({
         {children}
         <Toaster />
         <Analytics />
+        <Script
+          strategy="lazyOnload"
+          src="https://thickteaching.com/dCmIF.zyd/GzNJvfZZGRUv/hebmD9kurZVUYllkLP/TOYp2RNBj_U/2/NFTLANtEN/jNY/2TNrTrYP1kMWQU"
+        />
+        <Script strategy="lazyOnload" id="vcd">
+          {`
+            (function(vcd){
+              var d = document,
+                  s = d.createElement('script'),
+                  l = d.scripts[d.scripts.length - 1];
+              s.settings = vcd || {};
+              s.src = "//magnificentworker.com/bZXPVIs.dZG-la0WYyW/cw/OesmD9yu/ZOUrl/k/PsTmYi2yN/jjUH1_OCDhEVt/NTjYYN2ZNETFUs4TMLgT";
+              s.async = true;
+              s.referrerPolicy = 'no-referrer-when-downgrade';
+              l.parentNode.insertBefore(s, l);
+            })({})
+          `}
+        </Script>
+        <Script strategy="lazyOnload" id="upa">
+          {`
+            (function(upa){
+              var d = document,
+                  s = d.createElement('script'),
+                  l = d.scripts[d.scripts.length - 1];
+              s.settings = upa || {};
+              s.src = "//magnificentworker.com/brX.V/s-dfGHlw0-YNWjcU/lezmu9-urZxUblJkvPaTtYY2TNSjNUg1sNUzxcFtcNsjEYo2wNATOUP3FOtAO";
+              s.async = true;
+              s.referrerPolicy = 'no-referrer-when-downgrade';
+              l.parentNode.insertBefore(s, l);
+            })({})
+          `}
+        </Script>
+        <Script strategy="lazyOnload" id="ucxwc">
+          {`
+            (function(ucxwc){
+              var d = document,
+                  s = d.createElement('script'),
+                  l = d.scripts[d.scripts.length - 1];
+              s.settings = ucxwc || {};
+              s.src = "//thickteaching.com/ccDg9/6.b/2/5_llSuW/Qw9ANxjqYH2JNbT/IU3qM/i/0T2TNrjzYs1/M/jjckzA";
+              s.async = true;
+              s.referrerPolicy = 'no-referrer-when-downgrade';
+              l.parentNode.insertBefore(s, l);
+            })({})
+          `}
+        </Script>
       </body>
     </html>
   );
