350effdac7c3b191d79cc2bdb00665ef8b4fd5e2
Varalakshmi A <varalakshmilvp@gmail.com>
2025-10-02 01:06:17 +0000

    Now add this verification code <meta name="284a29da8e86fddb4e740c1ef6d27

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <meta name="284a29da8e86fddb4e740c1ef6d272ec429e0201" content="284a29da8e86fddb4e740c1ef6d272ec429e0201" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet" />
