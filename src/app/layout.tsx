c881b654e4e4fb2142f7245c611065c1309be5db
frixion0 <varalakshmilvp@gmail.com>
2025-10-05 13:56:06 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,7 @@ export default function RootLayout({
       <head>
         <meta name="referrer" content="no-referrer-when-downgrade" />
         <meta name="google-site-verification" content="r_Cs1I095TCgM9oTeFqXpnCRIDX28erq88hy1KZfTbM" />
+        <meta name="yandex-verification" content="981ded6b96311e58" />
       </head>
       <body className={`${inter.variable} font-body antialiased`}>
         {children}
